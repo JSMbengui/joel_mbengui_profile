cc26ae2a7b4fb79a1951b3b3cc6a61e01eed54b6
Joel Mbengui <joelsmbengui@gmail.com>
2023-03-28 18:18:48 +0200

    refactored the page sctruture

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,26 +7,26 @@ const MyApp: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Head>
-        <title>Ola | Joel Mbengui</title>
+        <title>Joel Sebastião Mbengui - Engenheiro de Software</title>
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="viewport" content="initial-scale=1, maximum-scale=1" />
 
-        <meta name="keywords" content="joelmbengui,programador" />
-        <meta name="description" content="Eu sou Joel Mbengui, engenheiro de software focado em criar aplicações para web e integração entre APIs." />
+        <meta name="keywords" content="joelmbengui,joel, sebastião, mbengui,engenheiro, software, web, web3.0, blockchain, programador" />
+        <meta name="description" content="Joel Sebastião Mbengui, engenheiro de software focado em criar aplicações para web e integração entre APIs." />
 
         {/* <!-- Facebook SEO --> */}
-        <meta property="og:title" content="Ola | Joel Mbengui" />
-        <meta property="og:description" content="Eu sou Joel Mbengui, engenheiro de software focado em criar aplicações para web e integração entre APIs." />
+        <meta property="og:title" content="Joel Sebastião Mbengui - Engenheiro de Software" />
+        <meta property="og:description" content="Joel Sebastião Mbengui, engenheiro de software focado em criar aplicações para web e integração entre APIs." />
         <meta property="og:type" content="website" />
         <meta property="og:image" content="/images/imagem.jpg" />
         <meta property="og:url" content="http://www.joelsmbengui.com" />
 
         {/* <!-- Twitter SEO --> */}
         <meta property="twitter:card" content="summary" />
-        <meta property="twitter:title" content="Ola | Joel Mbengui" />
-        <meta property="twitter:description" content="Eu sou Joel Mbengui, engenheiro de software focado em criar aplicações para web e integração entre APIs." />
+        <meta property="twitter:title" content="Joel Sebastião Mbengui - Engenheiro de Software" />
+        <meta property="twitter:description" content="Joel Sebastião Mbengui, engenheiro de software focado em criar aplicações para web e integração entre APIs." />
         <meta property="twitter:creator" content="@joelmbengui" />
         <meta property="twitter:site" content="@joelmbengui" />
         <meta property="twitter:image" content="/images/imagem.jpg" />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from 'next'
-import Home from '../client/pages/home'
+import Home from '../components/tampletes/home'
 
 const HomePage: NextPage = () => {
   return (
